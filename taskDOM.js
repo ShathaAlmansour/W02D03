5655790a1fb6085c5d62abd20ba0dbcd385ca49a
ShathaAlmansour <>
2021-10-20 00:02:37 +0300

    added file

---
+++ taskDOM.js
@@ -0,0 +1,16 @@
+const size =() => {
+const head = document.querySelectorAll("h1");
+head.forEach((element) =>
+element.style.size = "100%" }
+head.addEventListener("click",changestayl);
+
+const font = document.querySelector("#JavaScript");
+const p = () => font.style.fontFamily= "Tahoma";
+const change = document.querySelector("#JavaScript");
+change.addEventListener("click",p);
+
+const back = document.querySelector("#p2");
+const ground = (element) => back.style.backgroundColor= "#JavaScriptJavaScript";
+const highlight = document.querySelector("#JavaScriptJavaScript");
+highlight.addEventListener("click",ground);
+
