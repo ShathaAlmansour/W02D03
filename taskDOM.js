1d633997e0ca1e12a1aa584d82dd34b25f901914
ShathaAlmansour <>
2021-10-22 00:32:44 +0300

    added file

--- taskDOM.js
+++ taskDOM.js
@@ -1,33 +1,60 @@
-const size =() => {
-const head = document.querySelectorAll("h1");
-head.forEach((element) =>
-element.style.size = "100%" }
-head.addEventListener("click",changestayl);
+function size() {
+    let elements = document.querySelectorAll("h1");
+    elements.forEach((h1) => h1.style.fontSize = "40px")
+}
 
-const font = document.querySelector("#JavaScript");
-const p = () => font.style.fontFamily= "Tahoma";
-const change = document.querySelector("#JavaScript");
-change.addEventListener("click",p);
+function font() {
+    let elements = document.querySelectorAll("p");
+    elements.forEach((p) => p.style.fontFamily = "cursive"
+    );
+}
 
-const back = document.querySelector("#p2");
-const ground = (element) => back.style.backgroundColor= "#JavaScriptJavaScript";
-const highlight = document.querySelector("#JavaScriptJavaScript");
-highlight.addEventListener("click",ground);
+function Highlight() {
+    let elements = document.getElementById("JavaScript")
+    elements.style.backgroundColor = "grey";
+}
 
-const image = () => {
-    const image = document.getElementById("imge");
-    image.style.display = "none";
-  };
-  const hideImageButton = document.getElementById("imge");
-  hideImageButton.addEventListener("click", HideImage);
+function Hide() {
+    let elements = document.getElementById("img1")
+    elements.style.display = "none"
+}
 
-  
-  const originalBackground = () => {
-    paragraphs.forEach((element) => {
-      element.style.backgroundColor = "red";
+function Alternate() {
+    const img1 = document.getElementById("img1");
+    const img2 = document.getElementById("img2");
+    const img = img1.src;
+    img1.src = img2.src;
+    img2.src = img;
+}
+
+image = document.getElementById("img1");
+image.addEventListener("mouseover", function () {
+    image.src = "java3.jpg";
+});
+image = document.getElementById("img2");
+image.addEventListener("mouseover", function () {
+    image.src = "java3.jpg";
+});
+
+img1.addEventListener("contextmenu", function () {
+    alert("Alert");
+});
+
+const data = document.querySelectorAll("input").forEach((data) => {
+    data.addEventListener("change", function () {
+        alert("Your Data");
     });
-  };
-  paragraphs.forEach((element) => {
-    element.addEventListener("mouseover", changeBackground);
-    element.addEventListener("mouseleave", originalBackground);
-  });
+});
+
+function load() {
+    alert("Please Wait for loaded");
+}
+
+let p = document.querySelectorAll("p").forEach((p) => {
+    p.addEventListener("mouseover", function () {
+        p.style.backgroundColor = "pink";
+    });
+    p.addEventListener("mouseout", function () {
+        p.style.backgroundColor = "purple";
+    });
+});
